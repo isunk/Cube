38a3c1a3992d0263ce681a10337baa2127cd2b2c
904243852 <904243852@qq.com>
2025-08-18 11:53:02 +0800

    na

--- internal/handler/resource.go
+++ internal/handler/resource.go
@@ -10,10 +10,19 @@ import (
 func HandleResource(w http.ResponseWriter, r *http.Request) {
 	name := strings.TrimPrefix(r.URL.Path, "/resource/")
 
-	var content string
-	if err := internal.Db.QueryRow("select content from source where url = ? and type = 'resource' and active = true", name).Scan(&content); err != nil {
+	var (
+		content string
+		lang    string
+	)
+	if err := internal.Db.QueryRow("select content, lang from source where url = ? and type = 'resource' and active = true", name).Scan(&content, &lang); err != nil {
 		Error(w, err)
 		return
+	}
+	switch lang {
+	case "javascript":
+		w.Header().Set("Content-Type", "text/javascript; charset=utf-8")
+	case "html":
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	}
 	Success(w, content)
 }
